9faaae8d2260c8a80106a0f680e6bef853837d0f
phongtran <phongtranhk20@gmail.com>
2024-12-09 09:58:03 +0700

    add load and hashchange events

--- Project-7_Forkify/src/js/controller.js
+++ Project-7_Forkify/src/js/controller.js
@@ -32,12 +32,16 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if(!id) return;
+    
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e8673'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
@@ -163,4 +167,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
