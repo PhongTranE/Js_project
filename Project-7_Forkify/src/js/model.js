99542164478b9b82c88dcc2340dffe1e20cdecca
phongtran <phongtranhk20@gmail.com>
2024-12-11 14:38:51 +0700

    update recipe servings

--- Project-7_Forkify/src/js/model.js
+++ Project-7_Forkify/src/js/model.js
@@ -48,6 +48,8 @@ export const loadSearchResults = async function (query) {
         image: rec.image_url,
       };
     });
+
+    
   } catch (err) {
     console.log(`${err} 💥💥💥💥`);
     throw err;
@@ -61,4 +63,14 @@ export const getSearchResultsPage = function (page = state.search.page) {
   const end = page * state.search.resultsPerPage; //9;
 
   return state.search.results.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ing => {
+    ing.quantity = (ing.quantity * newServings) / state.recipe.servings;
+    // nweQt = oldQt * newServings / oldServings // 2 * 8 / 4 = 4
+  });
+
+
+  state.recipe.servings = newServings;
 };
